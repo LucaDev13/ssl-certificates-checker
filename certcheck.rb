58e2d707452c127bff618819c22da0171a2ba04e
Luca Melis <lucamelis@EUCs-MacBook-Pro.local>
2018-09-03 09:00:51 +0100

    amended messages to be slack attachments

--- certcheck.rb
+++ certcheck.rb
@@ -8,10 +8,9 @@ load "config.rb"
 #schedule the certificate check every day at 9am UCT
 scheduler = Rufus::Scheduler.new
     #example setting for test 
-      #scheduler.every '10s' do
-    #example setting for live 
-      #scheduler.cron '0 9 * * *' do
-
+-      #scheduler.every '10s' do
+-   #example setting for live 
+-      #scheduler.cron '0 9 * * *' do
 
 def ssl_check
     #performs the actual certificate check on the array of websites
@@ -32,19 +31,42 @@ def ssl_check
     #slack notifier settings with slack webhook
     notifier = Slack::Notifier.new @slack_webhook
 
-
+    #messages as slack attachments
+        plus_60 = {
+                              fallback: "*#{w}* will expire in over 60 days time. \n",
+                              text: " *#{w}* will expire in over 60 days time. \n",
+                              color: "#44bd32"
+                            }
+        eq_less_60 = {
+                              fallback: "Expiry date is #{days_left.round} days from now on #{cert_expiry_date}.",
+                              text: "Certificate check for *#{w}* : \n The certificate is valid. \n Expiry date is #{days_left.round} days from now on #{cert_expiry_date}. \n",
+                              color: "#3498db"
+            }
+        eq_less_30 = {
+                              fallback: "Expiry date is #{days_left.round} days from now on #{cert_expiry_date}.",
+                              text: "Certificate check for *#{w}*: \n The certificate is valid. \n Expiry date is #{days_left.round} days from now on #{cert_expiry_date}.\n Please start scheduling certificate renewal.",
+                              color: "#f0932b"
+            }
+        eq_less_10 = {
+                             fallback: "Expiry date is #{days_left.round} days from now on #{cert_expiry_date}.",
+                             text: "ATTENTION: Certificate check for *#{w}* : \n The certificate is valid. \n Expiry date is #{days_left.round} days days from now on #{cert_expiry_date}. Please act ASAP on the certificate renewal!  \n",
+                             color: "#c0392b"
+            }
     #logic to send out slack notification
      case
          when days_left <= 10
-                 notifier.ping "* ATTENTION Certificate check for #{w} : \n The certificate is valid. \n Expiry date is #{days_left.round} days days from now on #{cert_expiry_date}. Please act ASAP on the certificate renewal!  \n"
+                 notifier.post attachments: [eq_less_10]
+         
          when days_left <= 30
-                 notifier.ping "* Certificate check for #{w} : \n The certificate is valid. \n Expiry date is #{days_left.round} days from now on #{cert_expiry_date}.\n Please start scheduling certificate renewal."
+                 notifier.post attachments: [eq_less_30]
+    
          when days_left <= 60
-                 notifier.ping "* Certificate check for #{w} : \n The certificate is valid. \n Expiry date is #{days_left.round} days from now on #{cert_expiry_date}. \n"
+                 notifier.post attachments: [eq_less_60]
+         
          else
-
-                 notifier.ping "* #{w} will expire in over 60 days time. \n"
-         end
+                 notifier.post attachments: [plus_60]
+                
+     end
 
 end
 
